c438ba906737b2f042ff61df8125876465c0d4d9
carlo <carlolalas4@gmail.com>
2025-09-28 15:11:50 +0800

    add event functions

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+ const bntbrandX = document.querySelector(".addtocartbrandX");
+ const bntbrandX1 = document.querySelector(".addtocartbrandX1");
+ const bntbrandX2 = document.querySelector(".addtocartbrandX2");
+
+ bntbrandX.addEventListener("click", function() {
+    window.alert("BrandX has been added to your cart!!!");
+  });
+
+  bntbrandX1.addEventListener("click", function() {
+    window.alert("BrandX1 has been added to your cart!!!");
+  });
+
+  bntbrandX2.addEventListener("click", function() {
+    window.alert("BrandX2 has been added to your cart!!!");
+  });
+
+
+
+});
